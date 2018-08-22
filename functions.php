b3dfb1a9b5da4df50ef462ad7017411297b4c119
zhouliangshun <liangshunzhou@hotmail.com>
2018-08-22 23:26:16 +0800

    fixed url bugs

--- functions.php
+++ functions.php
@@ -214,7 +214,7 @@ function delete_data_server($table_name, $ids,$user){
     foreach ($_ids as $id){
         if(isset($id)){
             $wp_table_name = get_wp_table_name($table_name);
-            $result = $wpdb->get_results($wp_table_name, "SELECT local_id FROM $wp_table_name WHERE id = '$id' AND user = '$user'");
+            $result = $wpdb->get_results("SELECT local_id FROM $wp_table_name WHERE id = '$id' AND user = '$user'");
             echo json_encode($result);
             if(!$result){
                 continue;

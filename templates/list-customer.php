c9b322d3f3ea3f156fc7b7eac180f48dd2c75e73
zhouliangshun <liangshunzhou@hotmail.com>
2018-08-23 12:38:23 +0800

    modify customers

--- templates/list-customer.php
+++ templates/list-customer.php
@@ -6,44 +6,45 @@ include_once plugin_dir_path(__FILE__) . '../functions.php';
 <div id="wrap">
 
     <h1 class="wp-heading-inline"> <?= esc_html(get_admin_page_title()); ?> </h1>
-    <a href='javascript:onExport()' class="page-title-action">下单</a>|
-    <a href='javascript:onDelete("<?php $user = wp_get_current_user();echo esc_url(plugins_url('api/v1/customer/delete.php?user=', __FILE__ . 'sales-tools')) . $user->user_login ?>")' class="page-title-action">删除</a>
-        <table class="wp-list-table widefat fixed striped posts">
-            <thead>
-            <tr>
-                <td id="cb" class="manage-column column-cb check-column">
-                    <label class="screen-reader-text" for="cb-select-all">全选</label>
-                    <input id="cb-select-all" type="checkbox">
-                </td>
-                <th scope="col" id="author" class="manage-column column-author">姓名</th>
-                <th scope="col" id="title" class="manage-column column-title column-primary">
-                    <span>地址</span>
-                </th>
-                <th scope="col" id="phone" class="manage-column column-phone">
-                    <span>电话</span>
-                </th>
-            </tr>
-            </thead>
-            <tbody id="the-list">
-            <?php
+    <a class="page-title-action" href='javascript:onExport()'>下单</a>|
+    <a class="page-title-action" href='javascript:onDelete("<?php $user = wp_get_current_user();
+    echo esc_url(plugins_url('api/v1/customer/delete.php?user=', __FILE__ . 'sales-tools')) . $user->user_login ?>")'>删除</a>
+    <table class="wp-list-table widefat fixed striped posts">
+        <thead>
+        <tr>
+            <td id="cb" class="manage-column column-cb check-column">
+                <label class="screen-reader-text" for="cb-select-all">全选</label>
+                <input id="cb-select-all" type="checkbox">
+            </td>
+            <th scope="col" id="author" class="manage-column column-author">姓名</th>
+            <th scope="col" id="title" class="manage-column column-title column-primary">
+                <span>地址</span>
+            </th>
+            <th scope="col" id="phone" class="manage-column column-phone">
+                <span>电话</span>
+            </th>
+        </tr>
+        </thead>
+        <tbody id="the-list">
+        <?php
 
-            $user = wp_get_current_user();
-            $customers = get_customer_list($user->user_login);
-            if (isset($customers)) {
-                foreach ($customers as $customer) {
-                    echo "<tr id='$customer->server_id'>
+        $user = wp_get_current_user();
+        $customers = get_customer_list($user->user_login);
+        if (isset($customers)) {
+            foreach ($customers as $customer) {
+                echo "<tr id='$customer->server_id'>
                             <td><input id='cb-select-$customer->server_id' type='checkbox'></td>
-                            <th class = 'manage-column column-author name' style='width: 100%;height: 100%;padding: 10px 10px;'><input  type='text' value='$customer->name'/></th>
-                            <th class = 'manage-column column-title column-primary' style='width: 100%;height: 100%;padding: 10px 10px;'><input  type='text' value='$customer->address'/></th>
+                            <th class = 'manage-column column-author name' ><input  type='text' value='$customer->name' style='width: 100%;height: 100%;padding: 10px 10px;'/></th>
+                            <th class = 'manage-column column-title column-primary' ><input  type='text' value='$customer->address' style='width: 100%;height: 100%;padding: 10px 10px;'/></th>
                             <th class = 'manage-column column-phone phone'><input  type='text' style='width: 100%;height: 100%;padding: 10px 10px;' value='$customer->phone'/></th>
                           </tr>\n";
-                }
             }
+        }
 
-            ?>
-            </tbody>
-        </table>
-        <textarea align="center" placeholder="如无法自动复制请手动复制" id="data-text" style="display: none"></textarea>
+        ?>
+        </tbody>
+    </table>
+    <textarea align="center" placeholder="如无法自动复制请手动复制" id="data-text" style="display: none"></textarea>
 </div>
 
 <script>

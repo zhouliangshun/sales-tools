f08aa0ad9fc1ab803dd168a6253c88161ef2dcc5
zhouliangshun <shunliangzhou@live.cn>
2019-02-27 20:47:52 +0800

    fixed login

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
-    require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-load.php');
-    auth_redirect();
-    $user = wp_get_current_user();
-    $user_json = wp_json_encode(array('user' => $user->user_login,
-                                     'nickName' => $user->user_nicename,
-                                     'pic'=> $user->user_url));
-    echo "<html><script type='text/javascript'>Native.login('$user_json');</script></html>";
+require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-load.php');
+if (!is_user_logged_in()) auth_redirect();
+$user = wp_get_current_user();
+$user_json = wp_json_encode(array('user' => $user->user_login,
+    'nickName' => $user->user_nicename,
+    'pic' => $user->user_url));
+echo "<html><script type='text/javascript'>Native.login('$user_json');</script></html>";
 ?>
 
